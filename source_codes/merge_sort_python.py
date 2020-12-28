3f44d8aa7bc91ea2ec59d0f57f973d2b2f81b14a
sumanth <sumanthkumarrajus@gmail.com>
2020-12-28 01:35:31 -0500

    merge sort text file updated

--- source_codes/merge_sort_python.py
+++ source_codes/merge_sort_python.py
@@ -17,18 +17,24 @@ def merge_sort(arr):
 		while i < len(left_arr) and j < len(right_arr):
 
 			if left_arr[i] < right_arr[j]:
-
 				arr[k] = left_arr[i]
-
 				i += 1
 
 			else:
-
-				arr[k] = right_arr[i]
-
+				arr[k] = right_arr[j]
 				j += 1
 
 			k += 1
+
+		while i < len(left_arr):
+			arr[k] = left_arr[i]
+			i +=1
+			k+=1
+
+		while j < len(right_arr):
+			arr[k] = right_arr[j]
+			j +=1
+			k+=1
 
 	return arr
 
