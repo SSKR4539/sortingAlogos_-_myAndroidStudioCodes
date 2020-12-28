a9f5ab3ee24c8f2be322e3835dcf2b5179d1e0ce
sumanth <sumanthkumarrajus@gmail.com>
2020-12-28 01:22:27 -0500

    merge sort code updated

---
+++ source_codes/merge_sort_python.py
@@ -0,0 +1,36 @@
+def merge_sort(arr):
+	
+	if(len(arr)>1):
+
+		mid_index = len(arr)//2
+
+		left_arr = arr[:mid_index]
+
+		right_arr = arr[mid_index:]
+
+		merge_sort(left_arr)
+
+		merge_sort(right_arr)
+
+		i,j,k = 0,0,0
+
+		while i < len(left_arr) and j < len(right_arr):
+
+			if left_arr[i] < right_arr[j]:
+
+				arr[k] = left_arr[i]
+
+				i += 1
+
+			else:
+
+				arr[k] = right_arr[i]
+
+				j += 1
+
+			k += 1
+
+	return arr
+
+a = [14,55,23,76,89]
+print(merge_sort(a))
