76e0444039c1556935ab0b132c4b772448205eab
sumanth <sumanthkumarrajus@gmail.com>
2021-01-01 00:17:02 -0500

    selection_sort java code modified

--- source_codes/selection_sort_java.java
+++ source_codes/selection_sort_java.java
@@ -1,9 +1,25 @@
+class selection_sort_java
+{
+	public static void main(String args[])
+	{
 
+	int arr[] = {15, 75, 6, 0 ,64, 21, 100};
 
-public static main(){
-	
-	selection_sort(arr){
+	System.out.println("Array before sort:");
 
-}
+	printArray(arr);
+
+	System.out.println("Sorted array:");
+
+	printArray(arr);
+
+	}
+
+	static void printArray(int arr[])
+	{
+		for(int i=0;i<arr.length;i++)
+			{ 
+				System.out.print(arr[i] + " ");}
+	}
 
 }
